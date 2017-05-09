8bb64e62738489c8b2caea57fe429393574ac8c9
simple <1228115857@qq.com>
2017-05-09 11:43:13 +0800

    添加hangfire项目

---
+++ src/Maruko.Hangfire/Hangfire/HangfireManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+using Hangfire;
+
+namespace Maruko.Hangfire.Hangfire
+{
+    public static class HangfireManager
+    {
+        /// <summary>
+        /// 定时任务
+        /// </summary>
+        /// <param name="methodCall"></param>
+        /// <param name="cron"></param>
+        public static void TimingTask(Expression<Action> methodCall, string cron)
+        {
+            RecurringJob.AddOrUpdate(methodCall, cron);
+        }
+    }
+}
