98e6afb0af4a5bdd3d9de50b977c7025d738d9f7
phil <SkipperGuo@outlook.com>
2021-08-01 14:01:42 +0800

    zero 配置

--- zero/Maruko.Zero/Config/AppConfig.cs
+++ zero/Maruko.Zero/Config/AppConfig.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Autofac;
+using Maruko.Core.Extensions;
+using Microsoft.Extensions.Configuration;
 
 namespace Maruko.Zero.Config
 {
     public class AppConfig
     {
         public ZeroOption Zero { get; set; }
+
+        private IConfiguration configuration => ServiceLocator.Current.Resolve<IConfiguration>();
+
+        public AppConfig()
+        {
+            var section = configuration?.GetSection(nameof(Zero));
+            Zero = section.Exists()
+                ? section.Get<ZeroOption>()
+                : new ZeroOption();
+        }
     }
 }
