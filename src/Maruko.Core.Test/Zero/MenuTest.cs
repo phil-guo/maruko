7e8ab28fc916289185ef06860894af85eec426dc
phil <SkipperGuo@outlook.com>
2021-08-08 22:40:15 +0800

    111

---
+++ src/Maruko.Core.Test/Zero/MenuTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Maruko.Core.Extensions;
+using Maruko.Zero;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Maruko.Core.Test.Zero
+{
+    public class MenuTest : TestMarukoCoreBase
+    {
+        private readonly ISysMenuService _menu;
+        public MenuTest(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+            _menu = ServiceLocator.Current.Resolve<ISysMenuService>();
+        }
+
+        [Fact]
+        public void GetMenusByRole_Test()
+        {
+            var one = _menu.GetMenusByRole(new MenusRoleRequest()
+            {
+                RoleId = 1
+            });
+
+            Print(one);
+        }
+    }
+}
