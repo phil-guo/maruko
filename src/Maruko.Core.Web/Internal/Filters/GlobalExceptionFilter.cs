6682a64e6699bec254bdfc1f8ceba27c9440a2e3
phil <SkipperGuo@outlook.com>
2021-09-01 18:32:29 +0800

    全局异常捕捉器

---
+++ src/Maruko.Core.Web/Internal/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Maruko.Core.Application;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Maruko.Core.Web.Internal.Filters
+{
+    public class GlobalExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            base.OnException(context);
+
+            var result = new AjaxResponse<object>();
+
+        }
+    }
+}
