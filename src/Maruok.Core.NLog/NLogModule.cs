49899fc7a75a19741656c196b1fac31fa51bc6f6
phil <SkipperGuo@outlook.com>
2021-09-22 21:57:11 +0800

    添加Nlog类库

---
+++ src/Maruok.Core.NLog/NLogModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Autofac;
+using Maruko.Core.Extensions;
+using Maruko.Core.Modules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using NLog.Extensions.Logging;
+
+namespace Maruko.Core.NLog
+{
+    public class NLogModule : KernelModule
+    {
+        public override void Initialize(ILifetimeScope scope, IApplicationBuilder app)
+        {
+            //scope.Resolve<ILoggerFactory>().ConfigureNLog()
+        }
+
+        public override void ConfigureServices(IServiceCollection service)
+        {
+            service.AddLogging(logger =>
+            {
+                logger.AddConfiguration(ServiceLocator.Configuration.GetSection("Logging"));
+                logger.AddNLog();
+            });
+        }
+    }
+}
