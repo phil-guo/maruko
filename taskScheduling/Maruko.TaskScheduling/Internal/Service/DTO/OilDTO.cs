53fcd36cbbbf2df063b5118cc7110fe9ed4ccb22
phil <SkipperGuo@outlook.com>
2021-09-22 13:41:31 +0800

    修改创建时间赋值

--- taskScheduling/Maruko.TaskScheduling/Internal/Service/DTO/OilDTO.cs
+++ taskScheduling/Maruko.TaskScheduling/Internal/Service/DTO/OilDTO.cs
@@ -7,9 +7,13 @@ namespace Maruko.TaskScheduling
     [AutoMap(typeof(AllCountryOilPrice))]
     public class OilDTO : EntityDto
     {
+        public OilDTO()
+        {
+            CreateTime = DateTime.Now;
+        }
+
         public string CityName { get; set; }
         public string PriceJson { get; set; }
         public string NextNotify { get; set; }
-        public DateTime CreateTime { get; set; } = DateTime.Now;
     }
 }
