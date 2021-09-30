9fa6e3303425c4965b970277b71398109fc67b3a
phil <SkipperGuo@outlook.com>
2021-09-30 15:16:47 +0800

    通过角色获取菜单功能单元测试

---
+++ src/Maruko.Core.Test/Zero/OperateTest.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Maruko.Core.Extensions;
+using Maruko.Zero;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Maruko.Core.Test.Zero
+{
+    public class OperateTest : TestMarukoCoreBase
+    {
+        private readonly IOperateService _operate;
+
+
+        public OperateTest(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+            _operate = ServiceLocator.Current.Resolve<IOperateService>();
+        }
+
+        [Fact]
+        public void GetMenuOfOperate_Test()
+        {
+            var one = _operate.GetMenuOfOperate(new MenuOfOperateRequest()
+            {
+                MenuId = 11,
+                RoleId = 1
+            });
+
+            Print(one);
+        }
+    }
+}
