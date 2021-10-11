a581f22dbc948e23098feeb6ac6245f2752ec95f
phil.guo <1228115857@qq.com>
2021-10-11 23:53:13 +0800

    升级包版本

--- src/Maruko.Core.Web/Config/WebOption.cs
+++ src/Maruko.Core.Web/Config/WebOption.cs
@@ -9,5 +9,6 @@ namespace Maruko.Core.Web.Config
         public string Key { get; set; }
         public string Secret { get; set; }
         public int AuthExpired { get; set; }
+        public bool IsEnableAuth { get; set; }
     }
 }
