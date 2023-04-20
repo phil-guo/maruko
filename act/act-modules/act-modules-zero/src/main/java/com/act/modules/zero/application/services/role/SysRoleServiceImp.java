faf7e2bc599adbc7579a8a2c95c7c123c9caaa15
phil.guo <phil.guo@hg2mes.com>
2023-04-20 19:15:06 +0800

    角色重写删除方法

--- act/act-modules/act-modules-zero/src/main/java/com/act/modules/zero/application/services/role/SysRoleServiceImp.java
+++ act/act-modules/act-modules-zero/src/main/java/com/act/modules/zero/application/services/role/SysRoleServiceImp.java
@@ -1,6 +1,7 @@
 package com.act.modules.zero.application.services.role;
 
 import com.act.core.application.CurdAppService;
+import com.act.core.utils.FriendlyException;
 import com.act.modules.zero.application.services.role.dto.SetRolePermissionRequest;
 import com.act.modules.zero.application.services.user.dto.SysRoleDTO;
 import com.act.modules.zero.domain.SysRole;
@@ -10,7 +11,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class SysRoleServiceImp extends CurdAppService<SysRole, SysRoleDTO, SysRoleMapper> implements SysRoleService {
 
-    public Boolean setRolePermission(SetRolePermissionRequest request){
+    public Boolean setRolePermission(SetRolePermissionRequest request) {
         return false;
+    }
+
+    @Override
+    public void delete(Long id) throws FriendlyException {
+        if (id == 1)
+            throw new FriendlyException("超级管理员不允许被删除！");
+        super.delete(id);
     }
 }
