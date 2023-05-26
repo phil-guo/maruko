400fbbf519ac1d0d07fc6d3888d71501920f7511
phil.guo <phil.guo@hg2mes.com>
2023-05-26 13:35:32 +0800

    添加星图项目

---
+++ act/act-modules/act-modules-starmap/src/main/java/com/act/modules/starmap/StarMapApplication.java
@@ -0,0 +1,13 @@
+package com.act.modules.starmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarMapApplication.class, args);
+    }
+
+}
